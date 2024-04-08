723220e5c73e210d7f3a1da849f2904471c32f85
Deepak675-max <deepakkamboj.kamboj.675@gmail.com>
2024-04-08 11:10:24 +0530

    updated user db name

--- utils/database/init_mongodb.js
+++ utils/database/init_mongodb.js
@@ -1,7 +1,7 @@
 // Import Packages
 const mongoose = require("mongoose");
 
-const mongodbURI = 'mongodb://127.0.0.1:27017/user-service'
+const mongodbURI = 'mongodb://127.0.0.1:27017/user-db'
 
 // Establish Connection to MongoDB Server
 mongoose.set("strictQuery", false);
